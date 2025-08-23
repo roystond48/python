7a7f1298412427f748a9468b9e22f370d33f8672
roystond48 <roystond48@gmail.com>
2025-08-23 17:10:06 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,28 @@
+num1 = int(input("value1 :"))
+num2 = int(input("value2 :"))
+
+choice = int(input("enter 1 for +\nenter 2 for -\nenter 3 for /\nenter 4 for *\nenter 5 for %\nenter the choice :"))
+
+if(choice == 1):
+    print(f"{num1} + {num2} = {num1+num2}")
+elif(choice == 2):
+    print(f"({num1} - {num2} = {num1-num2}")
+elif(choice == 3):
+    print(f"{num1} / {num3} = {num1/num2}")
+elif(choice == 4):
+    print(f"{num1} * {num4} = {num1*num2}")
+elif(choice == 5):
+    print(f"{num1} % {num5} = {num1%num2}")
+
+             
+
+    
+    
+             
+        
+             
+           
+             
+             
+   
+             
